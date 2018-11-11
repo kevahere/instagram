e17096422fd0458095845ab6830c8a2ac921c3f2
kevahere <kevahere@gmail.com>
2018-11-11 17:43:17 +0300

    configured models

--- photofeed/models.py
+++ photofeed/models.py
@@ -1,11 +1,5 @@
 from django.db import models
-
-# Create your models here.
-class Profile(models.Model):
-    """Class that extends the user profile from django"""
-    user = models>ForeignKey(User,on_delete=models.CASCADE,default=1)
-    profile_pic = models.ImageField(upload_to='profiles/')
-    bio = models.CharField(max_length=250)
+from django.contrib.auth.models import User
 
 class Image(models.Model):
     """class defining the images"""
@@ -16,9 +10,18 @@ class Image(models.Model):
     likes = models.IntegerField(default=0)
     comments = models.ForeignKey(Comment,default=0)
 
+# Create your models here.
+class Profile(models.Model):
+    """Class that extends the user profile from django"""
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
+    profile_pic = models.ImageField(upload_to='profiles/')
+    bio = models.CharField(max_length=250)
+
 class Comment(models.Model):
     """Class that defines the comments"""
     user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     image = models.ForeignKey(Image,on_delete=models.CASCADE, default=1)
     comment = models.CharField(max_length=250)
     pub_time = models.DateTimeField(auto_now_add = True)
+
+
