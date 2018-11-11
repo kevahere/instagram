5eab75baed47d4db5c9c100efbf6ce5a9ba10f55
kevahere <kevahere@gmail.com>
2018-11-11 22:31:07 +0300

    added url for profile

--- photofeed/models.py
+++ photofeed/models.py
@@ -1,27 +1,94 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Image(models.Model):
-    """class defining the images"""
-    image = models.ImageField(upload_to='images/')
-    name = models.CharField(max_length=30)
-    caption = models.CharField(max_length=560)
-    profile = models.ForeignKey(Profile,default=1)
-    likes = models.IntegerField(default=0)
-    comments = models.ForeignKey(Comment,default=0)
 
-# Create your models here.
 class Profile(models.Model):
-    """Class that extends the user profile from django"""
-    user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
+    """
+    class that extends the user profile from django
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     profile_pic = models.ImageField(upload_to='profiles/')
     bio = models.CharField(max_length=250)
 
-class Comment(models.Model):
-    """Class that defines the comments"""
-    user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
-    image = models.ForeignKey(Image,on_delete=models.CASCADE, default=1)
+    @classmethod
+    def get_user(cls, user):
+        ask = cls.objects.filter(user=user)
+        return ask
+
+    @classmethod
+    def update_profile(cls, id, bio, pic):
+        upd8 = cls.objects.filter(user=id)
+        upd8.bio= bio
+        upd8.profile_pic = pic
+        upd8.save()
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def search_profiles(cls, search_term):
+        return cls.objects.filter(user__username__icontains=search_term)
+
+
+class Image(models.Model):
+    """
+    class that defines the images to be uploaded on the site
+    """
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=100)
+    post_date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def update_caption(self, update):
+        self.caption = update
+        self.save()
+
+    @classmethod
+    def delete_image(cls, id):
+        to_delete = cls.objects.filter(id=id)
+        to_delete.delete()
+
+    @classmethod
+    def get_by_user(cls, id):
+        return cls.objects.filter(user=id)
+
+    @classmethod
+    def get_all(cls):
+        return cls.objects.all().order_by('-id')
+
+    @classmethod
+    def search_image(cls, search_term):
+        return cls.objects.filter(caption__icontains=search_term)
+
+    @classmethod
+    def get_img_by_id(cls, ide):
+        picture = cls.objects.get(id=ide)
+        return picture
+
+
+class Comments(models.Model):
+    """
+    class that defines the post comments
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, default=1)
     comment = models.CharField(max_length=250)
-    pub_time = models.DateTimeField(auto_now_add = True)
+    pub_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 
+    def save_comment(self):
+        self.save()
+
+    @classmethod
+    def get_by_image(cls, id):
+        return cls.objects.filter(image=id)
--- photofeed/urls.py
+++ photofeed/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns =[
-    url('',views.index,name = 'index'),
+
+urlpatterns=[
+    url(r'^$',views.index,name = 'home'),
+    url(r'^profile/',views.profile, name='profile'),
+    url(r'^search_results/',views.search_results,name='search_results'),
+    url(r'^picture/(\d+)/$', views.single_pic, name='picture'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
