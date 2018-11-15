55c6fc9edce1183a81771cf3d34934fff7e17ada
kevahere <kevahere@gmail.com>
2018-11-15 11:44:38 +0300

    changed static configurations

--- insta/settings.py
+++ insta/settings.py
@@ -154,7 +154,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 django_heroku.settings(locals())
